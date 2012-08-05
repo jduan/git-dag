fdf90588d41a6134c5b542527a729557a645a8b4
Jingjing Duan <jingjing.duan@hulu.com>
2012-08-05 12:12:39 -0700

    mark branch names

--- app.rb
+++ app.rb
@@ -8,7 +8,8 @@ class GitGraph
   def initialize(git_repo)
     @repo = Grit::Repo.new(git_repo)
     @branches = @repo.branches
-    @remotes = @repo.remotes.reject {|r| r.name =~ /HEAD/}
+    @remotes = @repo.remotes
+    @all_branches = @remotes.reject {|r| r.name =~ /HEAD/} + @branches
   end
 
   def output_dot
@@ -20,12 +21,10 @@ class GitGraph
   def get_dot_str(all_commits)
     #puts "all_commits: #{all_commits.size}"
     dot_str = "digraph G {\n"
-    all_commits.each do |id, my_commit|
-      #puts "commit: #{my_commit.id}"
-      #puts "parent size: #{my_commit.parents.size}"
-      my_commit.parents.each do |parent|
-        dot_str << "#{my_commit.dot_node} -> #{parent.dot_node};\n"
-        dot_str << %Q(#{my_commit.dot_node} #{my_commit.label};\n)
+    all_commits.each do |id, node|
+      node.parents.each do |parent|
+        dot_str << "#{node.dot_node} -> #{parent.dot_node};\n"
+        dot_str << %Q(#{node.dot_node} #{node.label};\n)
         dot_str << %Q(#{parent.dot_node} #{parent.label};\n)
       end
     end
@@ -34,11 +33,10 @@ class GitGraph
 
   def build_entire_graph
     all_commits = {}
-    @remotes.each do |branch|
+    @all_branches.each do |branch|
       commits = find_commits_per_branch(branch.name)
-      commits.each_with_index do |commit, idx|
+      commits.each do |commit|
         my_commit = MyCommit.new(commit)
-        my_commit.branch = branch.name if idx == 0
         all_commits[commit.id] = my_commit unless all_commits.has_key? commit.id
         #puts "adding #{commit.id} to hash"
         commit.parents.each do |parent|
@@ -47,8 +45,19 @@ class GitGraph
           #all_commits[parent.id] = parent_commit unless all_commits.has_key? parent.id
         end
       end
+
+      parent_of_fake_head = all_commits[commits[0].id]
+      fake_head = create_fake_commit_for_head(branch.name, parent_of_fake_head)
+      all_commits[fake_head.id] = fake_head
     end
     all_commits
+  end
+
+  def create_fake_commit_for_head(branch_name, parent)
+    id = branch_name
+    dot_node = %Q("#{branch_name}")
+    label = %Q([color="blue" shape=box])
+    FakeHead.new(id, dot_node, label, [parent])
   end
 
   def find_commits_per_branch(branch)
@@ -56,9 +65,11 @@ class GitGraph
   end
 end
 
-class MyCommit
-  attr_accessor :parents, :branch
+class Node
+  attr_accessor :parents, :label, :id, :dot_node
+end
 
+class MyCommit < Node
   def initialize(grit_commit)
     @grit_commit = grit_commit
     @parents = []
@@ -68,26 +79,8 @@ class MyCommit
     %Q([color="#{color}"])
   end
 
-  def color
-    if merge?
-      "red"
-    elsif branch
-      "blue"
-    else
-      "black"
-    end
-  end
-
   def id
     @grit_commit.id
-  end
-
-  def short_sha1
-    @grit_commit.id[0,6]
-  end
-
-  def dot_node
-    %Q("#{short_sha1}")
   end
 
   def add_parent(parent)
@@ -96,9 +89,36 @@ class MyCommit
     #puts "parents: #{@parents}" if id =~ /07da/
   end
 
+  def dot_node
+    %Q("#{short_sha1}")
+  end
+
+  private
+  def color
+    if merge?
+      "red"
+    else
+      "black"
+    end
+  end
+
+  def short_sha1
+    @grit_commit.id[0,6]
+  end
+
   def merge?
     @parents.size > 1
   end
 end
+
+class FakeHead < Node
+  def initialize(id, dot_node, label, parents)
+    @id = id
+    @dot_node = dot_node
+    @label = label
+    @parents = parents
+  end
+end
+
 gg = GitGraph.new("~/workspace/cube-core")
 gg.output_dot
